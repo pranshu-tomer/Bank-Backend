ce1e288c44e5fbe4c2154ed34364a24fd906894b
pranshu-tomer <tomerpranshu11@gmail.com>
2025-09-12 17:19:19 +0530

    Uploading Files

---
+++ src/main/java/com/voltrex/bank/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.voltrex.bank.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(Map.of(
+                "success", false,
+                "error", ex.getMessage()
+        ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGeneral(Exception ex) {
+        return ResponseEntity.internalServerError().body(Map.of(
+                "success", false,
+                "error", "Something went wrong"
+        ));
+    }
+}
+
